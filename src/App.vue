58b7546efc25c777dc4edcb7ab7f026d41cfdbf9
ocavue <ocavue@gmail.com>
2017-12-17 07:45:20 +0800

    add mdc layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h2>Posts Links</h2>
-    <ul>
-      <li v-for="(post, index) in posts" :key="index">
-        <a :href="post.href">{{post.title}}</a>
-      </li>
-    </ul>
-  </div>
+  <mdc-layout-app class="app">
+    <mdc-toolbar class="app-toolbar" slot="toolbar" fixed>
+      <mdc-toolbar-row>
+        <mdc-toolbar-section align-start>
+          <mdc-toolbar-menu-icon event="toggle-drawer"></mdc-toolbar-menu-icon>
+          <mdc-toolbar-title>Drawer Exemple</mdc-toolbar-title>
+        </mdc-toolbar-section>
+      </mdc-toolbar-row>
+    </mdc-toolbar>
+
+    <mdc-drawer class="app-drawer" slot="drawer" toggle-on="toggle-drawer">
+      <mdc-drawer-header class="app-drawer-header">
+        TODO
+      </mdc-drawer-header>
+      <mdc-drawer-list>
+        <mdc-drawer-item href="#" start-icon="inbox">TODO</mdc-drawer-item>
+        <mdc-drawer-item href="#" start-icon="send">TODO</mdc-drawer-item>
+        <mdc-drawer-item href="#" start-icon="drafts">TODO</mdc-drawer-item>
+      </mdc-drawer-list>
+    </mdc-drawer>
+
+    <main class="app-content">
+      <div class="app-content-card-containter">
+        <mdc-card v-for="(post, index) in posts" :key="index" class="app-content-card" dark>
+          <a class="app-content-card-link" :href="post.href">
+            <mdc-card-media class="app-content-card--background" :height="320" :src="img">
+              <mdc-card-header class="app-content-card--title" :title="post.title" />
+            </mdc-card-media>
+          </a>
+        </mdc-card>
+      </div>
+    </main>
+  </mdc-layout-app>
 </template>
 
 <script>
@@ -18,13 +42,50 @@ export default {
   data () {
     return {
       posts: posts,
+      img: './src/assets/cat-1285634_640.png',
     }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+@import "theme.scss";
+
+body {
+  margin: 0;
+  background: $mdc-theme-background;
+}
+.app-drawer-header{
+  background: lightblue;  // TODO
+}
+.app-content {
+  margin: 16px;
+}
+.app-content-card {
+  background: white;
+  width: 100%;
+  max-width: 600px;
+  margin: 16px;
+}
+.app-content-card--title{
+  background: linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.5));
+  font-size: 2em;
+  color: white;
+}
+.app-content-card--background {
+  padding: 0;
+}
+.app-content-card-containter{
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+.app-content-card-link:link
+.app-content-card-link:visited
+.app-content-card-link:hover
+.app-content-card-link:active {
+  text-decoration: none;
+  // seem useless
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMDCAdapter from 'vue-mdc-adapter'
+// import './theme.scss'
+
+Vue.use(VueMDCAdapter)
 
 new Vue({
   el: '#app',
