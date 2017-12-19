899dd221c162ccace3e6c11ee9eaf8ce79792f5e
ocavue <ocavue@MBP.no_rep>
2017-12-19 22:37:37 +0800

    use `a la carte plugins`

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueMDCAdapter from 'vue-mdc-adapter'
 import router from './router'
-// import './theme.scss'
 
-Vue.use(VueMDCAdapter)
+import VueMDCButton from 'vue-mdc-adapter/dist/button'
+import VueMDCDrawer from 'vue-mdc-adapter/dist/drawer'
+import VueMDCToolbar from 'vue-mdc-adapter/dist/toolbar'
+import VueMDCLayoutApp from 'vue-mdc-adapter/dist/layout-app'
+import VueMDCCard from 'vue-mdc-adapter/dist/card'
+
+Vue.use(VueMDCButton)
+Vue.use(VueMDCDrawer)
+Vue.use(VueMDCToolbar)
+Vue.use(VueMDCLayoutApp)
+Vue.use(VueMDCCard)
 
 new Vue({
   el: '#app',
